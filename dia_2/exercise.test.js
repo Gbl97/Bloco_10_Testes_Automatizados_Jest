6381cf1eb35a34e30c4368f81df7827746593e93
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-04-20 20:42:02 -0300

    Exercicio 3

--- dia_2/exercise.test.js
+++ dia_2/exercise.test.js
@@ -30,4 +30,26 @@ describe('Teste dos Exercícios 2 e 3', () => {
             });
         });
     });
+
+    describe('Teste do Exercício 3', () => {
+        test('Quando existir o nome Mark Async', async () => {
+           const id4 = await getUserName(4).then(userName => {
+           expect(userName).toEqual('Mark');
+           });
+        });
+
+        test('Quando existir o nome Paul Async', async () => {
+           const id5 = await getUserName(5).then(userName => {
+           expect(userName).toEqual('Paul');
+           });
+        });
+
+        test('Quando não existir um nome Async', async () => {
+            try {
+              await getUserName(6);
+            } catch (error) {
+              expect(error).toEqual({ error: 'User with 6 not found.' });
+            }
+        });
+    });
 });
