208eec96a3be7b2228f05a03041545c4b3e2dc21
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-04-20 20:22:09 -0300

    Exercicio 2

--- dia_2/exercise.js
+++ dia_2/exercise.js
@@ -3,6 +3,29 @@ const uppercase = (str, callback) => {
   callback(str.toUpperCase());
 };
 
+// Exercicio 2 e 3 
+const users = {
+  4: { name: 'Mark' },
+  5: { name: 'Paul' }
+};
+
+const findUserById = (id) => {
+  return new Promise((resolve, reject) => {
+      if (users[id]) {
+        return resolve(users[id]);
+      };
+
+      return reject({ error: 'User with ' + id + ' not found.' });
+  });
+};
+
+const getUserName = (userID) => {
+  return findUserById(userID).then(user => user.name);
+};
+
 module.exports = {
     uppercase,
+    users,
+    findUserById,
+    getUserName,
 };
--- dia_2/exercise.test.js
+++ dia_2/exercise.test.js
@@ -1,10 +1,33 @@
-const { uppercase } = require('./exercise.js');
+const { uppercase, users, findUserById, getUserName } = require('./exercise.js');
 
-//Exercicio 1
+// Exercicio 1
 test('Verificando se a chamada da função callback transforma letras de uma palavra em maiúsculas', (done) => {
     const callback = (str) => {
         expect(str).toBe('HELLO');
         done()
     };
     uppercase('hello', callback);
+});
+
+// Exercicio 2 e 3
+describe('Teste dos Exercícios 2 e 3', () => {
+    describe('Teste do Exercício 2', () => {
+        test('Quando existir o nome Mark', () => {
+            return getUserName(4).then(userName => {
+                expect(userName).toEqual('Mark');
+            });
+        });
+
+        test('Quando existir o nome Paul', () => {
+            return getUserName(5).then(userName => {
+                expect(userName).toEqual('Paul');
+            });
+        });
+
+        test('Quando não existir um nome', () => {
+            return getUserName(6).catch(error => {
+                expect(error).toEqual({ error: 'User with 6 not found.' });
+            });
+        });
+    });
 });
