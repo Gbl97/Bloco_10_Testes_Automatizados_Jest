4de49a2298a7c42e8716d2f7970dba3b1d688253
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-04-23 10:21:13 -0300

    Exercicio 3

--- dia_3/exercise.test.js
+++ dia_3/exercise.test.js
@@ -35,3 +35,40 @@ test('Testes do Exercício 2', () => {
     expect(numberRandom).toHaveBeenCalledTimes(3);
 
 });
+
+// Exercício 3
+test('Testes do Exercício 3.1', () => {
+    numberRandom.mockImplementation((a, b, c) => a * b * c);
+
+    numberRandom(10, 5, 3);
+
+    // Testando se a função foi chamada
+    expect(numberRandom).toHaveBeenCalled();
+
+    // Testando se a função foi chamada com os parâmetros 10 e 5
+    expect(numberRandom).toHaveBeenCalledWith(10, 5, 3);
+
+    //Testando o retorno da função
+    expect(numberRandom(10, 5, 3)).toBe(150);
+
+    //Quantas vezes foi chamada
+    expect(numberRandom).toHaveBeenCalledTimes(5);
+});
+
+test('Testes do Exercício 3.2', () => {
+    numberRandom.mockImplementation((a) => a * 2);
+
+    numberRandom(10);
+
+    // Testando se a função foi chamada
+    expect(numberRandom).toHaveBeenCalled();
+
+    // Testando se a função foi chamada com os parâmetros 10 e 5
+    expect(numberRandom).toHaveBeenCalledWith(10);
+
+    //Testando o retorno da função
+    expect(numberRandom(10)).toBe(20);
+
+    //Quantas vezes foi chamada
+    expect(numberRandom).toHaveBeenCalledTimes(7);
+});
